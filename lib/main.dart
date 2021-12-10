e0f4d0f08dc4d4e0b860de0d6950f2f133cc5591
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-12-10 23:29:32 +0530

    removed debug sign

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Udemy',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
